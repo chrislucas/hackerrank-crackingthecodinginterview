990d6d8fb2b2cc366d4eabb6bb9b07c0ce4e04e0
Christoffer Lucas <christoffer.luccas@gmail.com>
2017-07-26 05:48:41 -0300

    estudo sobre hashinh

---
+++ CrackingCodeInterview/src/datastructure/IsBST.java
@@ -0,0 +1,13 @@
+package datastructure;
+
+/**
+ * Created by C_Luc on 22/07/2017.
+ *
+ * https://www.hackerrank.com/challenges/ctci-is-binary-search-tree?h_r=next-challenge&h_v=zen
+ *
+ */
+public class IsBST {
+
+
+
+}
---
+++ CrackingCodeInterview/src/datastructure/hashing/StudyHashing.java
@@ -0,0 +1,7 @@
+package datastructure.hashing;
+
+/**
+ * Created by C_Luc on 26/07/2017.
+ */
+public class StudyHashing {
+}
