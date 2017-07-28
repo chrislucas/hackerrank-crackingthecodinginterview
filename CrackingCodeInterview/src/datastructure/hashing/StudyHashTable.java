d7332f0fdd83e6f8d9f19770a2c10231583cb8f1
Christoffer Lucas <christoffer.luccas@gmail.com>
2017-07-28 06:35:16 -0300

    estudo sobre hashetable

--- CrackingCodeInterview/src/datastructure/hashing/StudyHashTable.java
+++ CrackingCodeInterview/src/datastructure/hashing/StudyHashTable.java
@@ -10,32 +10,55 @@ import java.util.List;
 
 public class StudyHashTable {
 
-    static class MHashtable<T> {
+    static class Hashingimpl1<T> implements CustomHashtable.Hashing<T> {
+        @Override
+        public int encode(T object) {
+            return 0;
+        }
+    }
+
+    static class CustomHashtable<T> {
 
         public interface Hashing<T> {
-            public int fnDisperse(T object);
+            public int encode(T object);
         }
 
         private List<List<T>> table;
         private Hashing<T> hashing;
         private static final int MIN_SIZE = 26;
 
-        public MHashtable(Hashing<T> hashing) {
+        public CustomHashtable(Hashing<T> hashing) {
             this.table = new ArrayList<>();
             for(int i=0; i<MIN_SIZE; i++)
                 table.add(new LinkedList<>());
         }
 
-        public void add(T object) {
+        public List<T> getList(T object) {
+            // gerando o codigo hash para esse elemento
+            int code = hashing.encode(object);
+            // pegando na tabela usando o codigo do elemento que se quer adicionar
+            List<T> list = table.get(code);
+            return list;
+        }
 
+        public void add(T object) {
+            if(contains(object))
+                return;
+            List<T> list = getList(object);
+            // adicionar o elemento a lista certa
+            list.add(object);
         }
 
         public void delete(T object) {
-
+            if(!contains(object))
+                return;
+            List<T> list = getList(object);
+            list.remove(object);
         }
 
         public boolean contains(T object) {
-            return false;
+            List<T> list = getList(object);
+            return list.contains(object);
         }
 
         public List<T> list() {
