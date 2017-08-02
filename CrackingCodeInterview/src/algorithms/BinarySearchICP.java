444bd20d85c313855d5d0282ae67242be0a4c326
chrislucas <christoffer.luccas@gmail.com>
2017-08-02 18:25:55 -0300

    exercicio besta

---
+++ CrackingCodeInterview/src/algorithms/BinarySearchICP.java
@@ -0,0 +1,15 @@
+package algorithms;
+
+/**
+ * Created by r028367 on 02/08/2017.
+ * https://www.hackerrank.com/challenges/ctci-ice-cream-parlor
+ */
+public class BinarySearchICP {
+
+    static int  [] ids;
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- CrackingCodeInterview/src/datastructure/HashTableRansomNote.java
+++ CrackingCodeInterview/src/datastructure/HashTableRansomNote.java
@@ -1,8 +1,55 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
 /**
  * Created by r028367 on 20/07/2017.
  * https://www.hackerrank.com/challenges/ctci-ransom-note
+ * DONE
+ * https://dzone.com/articles/hashmap-vs-treemap-vs
  */
 public class HashTableRansomNote {
+    //private static Map<String, Integer> table = new HashMap<>();
+    //private static Map<String, Integer> table = new LinkedHashMap<>();
+    private static Map<String, Integer> table = new Hashtable<>();
+    public static void solver() {
+        int qWordsInTable, qWordsInRansomNote;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            StringTokenizer tk = new StringTokenizer(bufferedReader.readLine(), " ");
+            qWordsInTable = Integer.parseInt(tk.nextToken());
+            qWordsInRansomNote = Integer.parseInt(tk.nextToken());
+            tk = new StringTokenizer(bufferedReader.readLine(), " ");
+            while(tk.hasMoreTokens()) {
+                String word = tk.nextToken();
+                if(table.containsKey(word)) {
+                    int q = table.get(word);
+                    table.put(word, q+1);
+                }
+                else  {
+                    table.put(word, 1);
+                }
+            }
+            boolean ctn = true;
+            tk = new StringTokenizer(bufferedReader.readLine(), " ");
+            while(tk.hasMoreTokens()) {
+                String word = tk.nextToken();
+                if(table.containsKey(word) /*&& table.get(word) > 0*/) {
+                    int q = table.get(word);
+                    table.put(word, --q);
+                    if(q == 0)
+                        table.remove(word);
+                }
+                else {
+                    ctn = false;
+                }
+            }
+            System.out.println(ctn ? "Yes" : "No");
+        } catch (Exception e) {}
+    }
+    public static void main(String[] args) {
+        solver();
+    }
 }
