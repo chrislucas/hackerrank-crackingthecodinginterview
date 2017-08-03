b5ca51df627045742cee135cb589eecbab0a9cde
chrislucas <christoffer.luccas@gmail.com>
2017-08-03 18:50:20 -0300

    exdercicio sobre bsearch

---
+++ CrackingCodeInterview/src/algorithms/TwoSumProblems.java
@@ -0,0 +1,10 @@
+package algorithms;
+
+/**
+ * Created by r028367 on 03/08/2017.
+ * https://leetcode.com/articles/two-sum/
+ * https://algorithmstuff.wordpress.com/2013/07/30/two-sum-problem/
+ * https://en.wikipedia.org/wiki/3SUM
+ */
+public class TwoSumProblems {
+}
---
+++ CrackingCodeInterview/src/techniques/Bitmanipulation.java
@@ -0,0 +1,8 @@
+package techniques;
+
+/**
+ * Created by r028367 on 03/08/2017.
+ * https://www.hackerrank.com/challenges/ctci-lonely-integer
+ */
+public class Bitmanipulation {
+}
