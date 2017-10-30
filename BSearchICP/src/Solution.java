826614cebce44854cf00f7cd3ea7d8468c534a2d
chrislucas <christoffer.luccas@gmail.com>
2017-10-30 18:18:21 -0200

    commit

---
+++ BSearchICP/src/Solution.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * DONE
+ * https://www.hackerrank.com/challenges/ctci-ice-cream-parlor/problem
+ * Resolver usando Two Sum como indicado na discussao
+ * https://leetcode.com/articles/two-sum/
+ * */
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
