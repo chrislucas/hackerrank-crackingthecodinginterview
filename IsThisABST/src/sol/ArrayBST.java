5a8b6b61ea80fe1101abbcca775b1dcbfc60b210
chrislucas <christoffer.luccas@gmail.com>
2017-10-31 18:01:41 -0200

    commit

--- IsThisABST/src/sol/ArrayBST.java
+++ IsThisABST/src/sol/ArrayBST.java
@@ -1,4 +1,58 @@
 package sol;
 
+
+/**
+ * https://www.hackerrank.com/challenges/ctci-is-binary-search-tree/forum
+ * */
 public class ArrayBST {
+
+    public static class Node<Comparable> {
+        Comparable data;
+        Node left, right;
+
+        public Node(Comparable data) {
+            this.data   = data;
+            this.left   = null;
+            this.right  = null;
+        }
+
+        @Override
+        public String toString() {
+            return String.format(data.toString());
+        }
+
+
+    }
+
+    public Node<Comparable> sortedArrayToBST(Comparable [] data, int s, int e) {
+        if(s >= e) {
+            return null;
+        }
+        int mid = (e-s)/2+s;
+        Node node   = new Node(data[mid]);
+        node.left   = sortedArrayToBST(data, s, mid-1);
+        node.right  = sortedArrayToBST(data, mid+1, e);
+        return node;
+    }
+
+    public void print(Node node) {
+        System.out.println(node);
+    }
+
+    public void preorder(Node root) {
+        if(root != null) {
+            print(root);
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Comparable [] array = {10,20,30,40,50,60,70};
+        ArrayBST inst = new ArrayBST();
+        Node node = inst.sortedArrayToBST(array, 0, array.length);
+        inst.print(node);
+    }
+
+
 }
--- kdtree/src/KDimensionalTree.java
+++ kdtree/src/KDimensionalTree.java
@@ -1,2 +1,28 @@
 public class KDimensionalTree {
+
+
+    public class Point2D {
+        private int x,y;
+        public Point2D(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public class Node {
+        private Point2D p;
+        private Node left, right;
+        public Node(Point2D p) {
+            this.p = p;
+        }
+    }
+
+
+    public Node root;
+
+
+    public static void main(String[] args) {
+
+    }
+
 }
