545dab8f0d0ec8f76142b776e4a766171a5265b2
chrislucas <christoffer.luccas@gmail.com>
2017-10-31 18:13:04 -0200

    adicionando algumas coisas para adiantar

--- kdtree/src/KDimensionalTree.java
+++ kdtree/src/KDimensionalTree.java
@@ -17,8 +17,26 @@ public class KDimensionalTree {
         }
     }
 
+    private Node root;
+    private Node add(Node root, Point2D p) {
+        return root;
+    }
 
-    public Node root;
+    public  void test() {
+        Point2D [] points2D = {
+             new Point2D(3, 6)
+            ,new Point2D(17, 15)
+            ,new Point2D(13, 15)
+            ,new Point2D(6, 12)
+            ,new Point2D(9, 1)
+            ,new Point2D(2, 7)
+            ,new Point2D(10, 19)
+        };
+
+        for(Point2D p: points2D)
+            add(root, p);
+
+    }
 
 
     public static void main(String[] args) {
