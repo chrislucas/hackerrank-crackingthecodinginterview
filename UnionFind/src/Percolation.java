4182546fe04aa04f8c6bf7e3630e039a941f60bf
chrislucas <christoffer.luccas@gmail.com>
2017-11-08 11:53:48 -0200

    resolvendo exercicio percolation

--- UnionFind/src/Percolation.java
+++ UnionFind/src/Percolation.java
@@ -13,45 +13,94 @@ public class Percolation {
      * */
 
     public Percolation(int n) {
+        if(n < 0) {
+            throw new IllegalArgumentException();
+        }
         qSites  = n;
-        int limit = (n+2)*(n+2);
-        sites   = new boolean[limit+1];
-        ds      = new int[limit+1];
-        sz      = new int[limit+1];
+        int limit = n*n+2;
+        sites   = new boolean[limit];
+        ds      = new int[limit];
+        sz      = new int[limit];
         for(int i = 0; i<limit; i++) {
             ds[i] = i;
             sz[i] = 1;
         }
         // os 2 indices abaixo sao auxiliares
-        //sites[0] = true;
-        //sites[szMatrix-1] = true;
+        sites[0] = true;
+        sites[limit-1] = true;
     }
 
     private int getIndex(int r, int c) {
-        return r * qSites + c;
+        return (r-1) * (qSites) + c;
     }
 
     public void open(int row, int col) {
-        if(row < 1 || row > qSites || col < 1 || col > qSites) {
+        if(!isValid(row, col)) {
             throw new IllegalArgumentException();
         }
-        if(!isOpen(row, col)) {
-            int idx = getIndex(row, col);
-            sites[idx] = true;
+        if(!isOpen(row, col)) { }
+        int idx = getIndex(row, col);
+        if(row == 1) {
+            union(idx, ds[0]);
         }
+        else if(row == qSites) {
+            union(idx, ds[qSites*qSites+1]);
+        }
+        for(int k=0; k<directions.length; k++) {
+            int i = directions[k][0] + row;
+            int j = directions[k][1] + col;
+            if(isValid(i, j) && isOpen(i, j)) {
+                union(getIndex(i, j), idx);
+            }
+        }
+        sites[idx] = true;
+    }
+
+    private void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ)
+            return;
+        ds[rootP] = rootQ;
+    }
+
+    private int find(int p) {
+        if(p != ds[p]) {
+            ds[p] = ds[ds[p]];
+            return find(ds[p]);
+        }
+        return p;
+    }
+
+    private boolean connected(int p, int q) {
+        return find(p) == find(q);
     }
 
     public boolean isOpen(int row, int col) {
-        if(row < 1 || row > qSites || col < 1 || col > qSites) {
+        if(!isValid(row, col)) {
             throw new IllegalArgumentException();
         }
         int idx = getIndex(row, col);
         return sites[idx];
     }
 
+    private static final int [][] directions = {
+         {0,-1}    // left
+        ,{-1,0}    // top
+        ,{0,1}     // right
+        ,{1,0}     // bottom
+    };
+
+    private boolean isValid(int row, int col) {
+        return row > 0 && row < qSites+1 && col > 0 && col < qSites+1;
+    }
+
     public boolean isFull(int row, int col) {
-        if(row < 1 || row > qSites || col < 1 || col > qSites) {
-            throw new IllegalArgumentException();
+        if(!isValid(row, col)) {
+            throw new IllegalArgumentException(String.format("%d %d", row, col));
+        }
+        if(isOpen(row, col)) {
+            return connected(ds[0], getIndex(row, col));
         }
         return false;
     }
@@ -61,7 +110,7 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return false;
+        return find(ds[0]) == find(ds[qSites*qSites+1]);
     }
 
     public static void main(String[] args) { }
--- UnionFind/src/PercolationVisualizer.java
+++ UnionFind/src/PercolationVisualizer.java
@@ -52,14 +52,16 @@ public class PercolationVisualizer {
         StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 12));
         StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.text(0.25*n, -0.025*n, opened + " open sites");
-        if (perc.percolates()) StdDraw.text(0.75*n, -0.025*n, "percolates");
-        else                   StdDraw.text(0.75*n, -0.025*n, "does not percolate");
+        if (perc.percolates())
+            StdDraw.text(0.75*n, -0.025*n, "percolates");
+        else
+            StdDraw.text(0.75*n, -0.025*n, "does not percolate");
     }
 
     public static void main(String[] args) {
-        String path = "raw/testing/percolation/input50.txt";
-        In in = new In(path);      // input file
-        int n = in.readInt();         // n-by-n percolation system
+        String path = "raw/testing/percolation/snake101.txt";
+        In in = new In(path);           // input file
+        int n = in.readInt();           // n-by-n percolation system
         // turn on animation mode
         StdDraw.enableDoubleBuffering();
         // repeatedly read in sites to open and draw resulting system
