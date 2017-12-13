e12ef78ba4d5c588d120a43f0ddebabe28c4d723
chrislucas <christoffer.luccas@gmail.com>
2017-12-13 18:07:44 -0200

    estudos

--- CrackingCodeInterview/src/datastructure/HeapFindRunningMedian.java
+++ CrackingCodeInterview/src/datastructure/HeapFindRunningMedian.java
@@ -1,11 +1,19 @@
 package datastructure;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 /**
  * https://www.hackerrank.com/challenges/ctci-find-the-running-median/problem
  *
  * */
 public class HeapFindRunningMedian {
+
+    public static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static final PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out), true);
 
     public static void main(String[] args) {
 
--- CrackingCodeInterview/src/datastructure/LinkedListDetectCycle.java
+++ CrackingCodeInterview/src/datastructure/LinkedListDetectCycle.java
@@ -3,11 +3,85 @@ package datastructure;
 
 /**
  * https://www.hackerrank.com/challenges/ctci-linked-list-cycle/problem
+ * DONE
  * */
 public class LinkedListDetectCycle {
 
-    public static void main(String[] args) {
+    class Node {
+        int data;
+        Node next;
+    }
 
+    private static LinkedListDetectCycle linkedListDetectCycle = new LinkedListDetectCycle();
+
+    public Node head, tail;
+
+    boolean hasCycle(Node head) {
+        return s1(head);
+    }
+
+    public boolean s1(Node head) {
+        Node slow = head, fast = head;
+        while( slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+
+    public void add(Node node) {
+        if(head == null) {
+            tail = node;
+            tail.data = node.data;
+            head = tail;
+        }
+        else {
+            Node copy = tail;
+            tail = node;
+            tail.data = node.data;
+            copy.next = tail;
+        }
+    }
+
+    public void removeAll() {
+        while (head != null)
+            head = head.next;
+        tail = null;
+    }
+
+
+    public void start() {
+
+        Node [] nodesA = {
+             new Node()
+            ,new Node()
+            ,new Node()
+        };
+        nodesA[0].data = 1;
+        nodesA[1].data = 2;
+        nodesA[2].data = 3;
+        for(Node node : nodesA)
+            linkedListDetectCycle.add(node);
+        System.out.println(linkedListDetectCycle.hasCycle(head));
+        removeAll();
+
+        Node [] nodesB = new Node[3];
+        nodesB[0] = new Node();
+        nodesB[0].data = 1;
+        nodesB[1] = new Node();
+        nodesB[1].data = 2;
+        nodesB[2] = nodesB[0];
+
+        for(Node node : nodesB)
+            linkedListDetectCycle.add(node);
+        System.out.println(linkedListDetectCycle.hasCycle(head));
+    }
+
+
+    public static void main(String[] args) {
+        linkedListDetectCycle.start();
     }
 
 }
