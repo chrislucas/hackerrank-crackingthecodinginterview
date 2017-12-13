9fb588bdc3cfd2eb40567ab34e8ba675cb6a60bd
chrislucas <christoffer.luccas@gmail.com>
2017-12-13 10:26:35 -0200

    merging

---
+++ CrackingCodeInterview/src/datastructure/HeapFindRunningMedian.java
@@ -0,0 +1,13 @@
+package datastructure;
+
+
+/**
+ * https://www.hackerrank.com/challenges/ctci-find-the-running-median/problem
+ *
+ * */
+public class HeapFindRunningMedian {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ CrackingCodeInterview/src/datastructure/LinkedListDetectCycle.java
@@ -0,0 +1,13 @@
+package datastructure;
+
+
+/**
+ * https://www.hackerrank.com/challenges/ctci-linked-list-cycle/problem
+ * */
+public class LinkedListDetectCycle {
+
+    public static void main(String[] args) {
+
+    }
+
+}
